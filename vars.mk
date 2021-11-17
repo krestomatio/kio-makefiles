556c822b0a6b1071fea7e7585bf3f4767d3dfaea
Job Céspedes Ortiz <jobcespedes@gmail.com>
2021-11-16 22:28:33 -0600

    fix: do not use test subindex if no pull number given

--- vars.mk
+++ vars.mk
@@ -67,7 +67,11 @@ LAST_TAG ?= $(shell git describe --tags --abbrev=0 2> /dev/null || echo)
 # molecule
 MOLECULE_SEQUENCE ?= test
 MOLECULE_SCENARIO ?= default
+ifeq ($(PULL_NUMBER),0)
+TEST_SUBINDEX := 0
+else
 TEST_SUBINDEX := $(shell date +%s | tail -c 3)
+endif
 export OPERATOR_IMAGE ?= $(IMG)
 export TEST_OPERATOR_NAMEPREFIX ?= $(OPERATOR_SHORTNAME)-$(JOB_NAME)-$(PULL_NUMBER)-$(TEST_SUBINDEX)-
 export TEST_OPERATOR_NAMESPACE ?= $(OPERATOR_SHORTNAME)-$(JOB_NAME)-$(PULL_NUMBER)-$(TEST_SUBINDEX)-ns
