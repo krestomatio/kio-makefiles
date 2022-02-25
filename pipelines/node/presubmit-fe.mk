3217f7a249b1516c72b5c680442ba37c992b0fe2
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-02-25 17:32:03 -0600

    refactor(kio-web-app): remove npm-install from pipeline

--- pipelines/node/presubmit-fe.mk
+++ pipelines/node/presubmit-fe.mk
@@ -1,5 +1,3 @@
-pr-npm: npm-ci npm-build ## NPM tasks during pr pipeline
-
 pr-image: testing-buildah-image ## Image build, push during pr pipeline
 
 lint: npm-ci npx-commitlint npm-lint npm-pretty ## Project linting pipeline
