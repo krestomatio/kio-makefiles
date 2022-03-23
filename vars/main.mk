acf5cb0fdd43a08e5dccce91526d95b2d531ce78
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-03-22 18:30:15 -0600

    fix: change release branch detection

--- vars/main.mk
+++ vars/main.mk
@@ -87,7 +87,7 @@ GIT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD 2>/dev/null)
 endif
 GIT_LAST_TAG ?= $(shell git describe --tags --abbrev=0 2> /dev/null || echo)
 GIT_ADD_FILES ?= Makefile
-GIT_RELEASE_BRANCH_NUMBER ?= $(shell echo $(GIT_BRANCH) | grep -q '^release.[[:digit:]].[[:digit:]]' && echo $(GIT_BRANCH:release-%=%) || echo)
+GIT_RELEASE_BRANCH_NUMBER ?= $(shell echo $(GIT_BRANCH) | grep -qE '^release-([0-9]+)\.([0-9]+)$$' && echo $(GIT_BRANCH:release-%=%) || echo)
 CHANGELOG_FILE ?= CHANGELOG.md
 CHANGELOG_LAST_TAG ?= $(GIT_LAST_TAG)
 
