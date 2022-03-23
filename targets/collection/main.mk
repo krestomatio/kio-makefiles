4ab69820bdff57e2b710fba13bc8991dc0a6b292
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-03-22 20:06:56 -0600

    feat: omit collection upload if already so

--- targets/collection/main.mk
+++ targets/collection/main.mk
@@ -1,5 +1,9 @@
 ##@ Collection
 
+GALAXY_COLLECTION_NAME ?= krestomatio-k8s-$(VERSION)
+GALAXY_COLLECTION_FILE ?= $(GALAXY_COLLECTION_NAME).tar.gz
+GALAXY_COLLECTION_DOWNLOAD_URL ?= https://galaxy.ansible.com/download/$(GALAXY_COLLECTION_FILE)
+GALAXY_COLLECTION_PUBLISHED := $(shell curl --output /dev/null --silent --head --fail "$(GALAXY_COLLECTION_DOWNLOAD_URL)" && echo true || echo false)
 
 .PHONY: galaxy-version
 galaxy-version: ## Bump galaxy version
@@ -10,9 +14,13 @@ galaxy-version: ## Bump galaxy version
 .PHONY: galaxy-publish
 galaxy-publish: ## Publish galaxy collection
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
+ifeq ($(GALAXY_COLLECTION_PUBLISHED),false)
 	ansible-galaxy collection build --force
 	$(info ansible-galaxy collection publish krestomatio-k8s-$(VERSION).tar.gz)
 	@ansible-galaxy collection publish krestomatio-k8s-$(VERSION).tar.gz --api-key $(ANSIBLE_GALAXY_TOKEN)
+else
+	@echo -e "not publisging $(GALAXY_COLLECTION_FILE), it already already is"
+endif
 
 .PHONY: ansible-lint
 ansible-lint: ## Ansible linting
