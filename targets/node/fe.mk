e0dacac8f31fcfe42ab85f541c3b9d2a2d45afa7
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-05-05 20:17:39 -0600

    fix(kio-web-app): change vault field

--- targets/node/fe.mk
+++ targets/node/fe.mk
@@ -5,9 +5,9 @@
 config-js-download-if: vault ## download and overwrite config.js file for kio web app api, but only if it does not exist on disk
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 ifeq (,$(wildcard .env))
-	vault kv get -field kio-api-env kio_secrets/kio-web-app > public/config.js
+	vault kv get -field config.js kio_secrets/kio-web-app > public/config.js
 endif
 
 config-js-download: vault ## download and overwrite config.js file for kio web app api
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
-	vault kv get -field kio-api-env kio_secrets/kio-web-app > public/config.js
+	vault kv get -field config.js kio_secrets/kio-web-app > public/config.js
