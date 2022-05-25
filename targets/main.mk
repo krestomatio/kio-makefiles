b7fc395421391aaf07539d5253a4948406313bee
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-05-25 10:00:51 -0600

    fix: do not include help and kustomize in go-operator

--- targets/main.mk
+++ targets/main.mk
@@ -5,6 +5,7 @@ ifneq (,$(wildcard $(MK_TARGETS_PROJECT_TYPE_FILE)))
 include $(MK_TARGETS_PROJECT_TYPE_FILE)
 endif
 
+ifneq ($(PROJECT_TYPE),go-operator)
 help: ## Display this help.
 	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
 
@@ -23,6 +24,7 @@ ifeq (,$(shell which kustomize 2>/dev/null))
 	}
 else
 KUSTOMIZE = $(shell which kustomize)
+endif
 endif
 endif
 
