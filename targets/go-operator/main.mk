bb141dbfcf8ef0719136f5b777260370cd470ca9
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-05-27 21:23:35 -0600

    fix(kio): add go install func

--- targets/go-operator/main.mk
+++ targets/go-operator/main.mk
@@ -1,14 +1,23 @@
 ##@ Go
 
+# go-install will 'go install' any package $2 to $1.
+define go-install
+@[ -f $(1) ] || { \
+set -e ;\
+TMP_DIR=$$(mktemp -d) ;\
+cd $$TMP_DIR ;\
+go mod init tmp ;\
+echo "Downloading $(2)" ;\
+GOBIN=$(shell dirname $$(realpath $(1))) go install $(2) ;\
+rm -rf $$TMP_DIR ;\
+}
+endef
+
 .PHONY: go-lint
-HAS_GOLINT := $(shell which $(PROJECT_DIR)/bin/golangci-lint)
 go-lint: ## Verifies `golint` passes
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
-ifndef HAS_GOLINT
-	$(call go-get-tool,$(PROJECT_DIR)/bin/golangci-lint,github.com/golangci/golangci-lint/cmd/golangci-lint@v1.26.0)
-endif
+	$(call go-install,$(LOCAL_BIN)/golangci-lint,github.com/golangci/golangci-lint/cmd/golangci-lint@v1.26.0)
 	@bin/golangci-lint run --timeout 5m
-
 
 .PHONY: kio-go-cache
 kio-go-cache: ## Verifies `golint` passes
