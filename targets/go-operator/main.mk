6c73349d69270064ff15f150f2f5c35e99f9589f
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-06-28 19:13:21 -0600

    chore(go-operator: bump go-lint version)

--- targets/go-operator/main.mk
+++ targets/go-operator/main.mk
@@ -16,14 +16,14 @@ endef
 .PHONY: go-lint
 go-lint: ## Verifies `golint` passes
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
-	$(call go-install,$(LOCAL_BIN)/golangci-lint,github.com/golangci/golangci-lint/cmd/golangci-lint@v1.26.0)
-	@bin/golangci-lint run --timeout 5m
+	$(call go-install,$(LOCAL_BIN)/golangci-lint,github.com/golangci/golangci-lint/cmd/golangci-lint@v1.46.2)
+	bin/golangci-lint run --timeout 5m
 
 .PHONY: kio-go-cache
 kio-go-cache: ## Verifies `golint` passes
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	mkdir -p /shared/operator-sdk.$(OPERATOR_VERSION)/go/{bin,testbin,.cache}
-	ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/bin/ bin
-	ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/testbin/ testbin
-	ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/ ~/go
-	ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/.cache/ ~/.cache
+	test -h $${HOME:-~}/go || ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/ $${HOME:-~}/go
+	test -h bin || ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/bin/ bin
+	test -h testbin || ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/testbin/ testbin
+	test -h $${HOME:-~}/.cache || ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/.cache/ $${HOME:-~}/.cache
