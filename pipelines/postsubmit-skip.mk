a22221ce1a5521dcc12c388f4c3066de67ddeac2
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-07-04 13:00:02 -0600

    refactor: add phony and multiarch to release

--- pipelines/postsubmit-skip.mk
+++ pipelines/postsubmit-skip.mk
@@ -1,15 +1,24 @@
+.PHONY: multiarch-image
+multiarch-image:
+	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
+	$(info skipping multiarch-image...)
+
+.PHONY: changelog
 changelog:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping changelog...)
 
+.PHONY: preview
 preview:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping preview...)
 
+.PHONY: release
 release:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping release...)
 
+.PHONY: promote
 promote:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping promote...)
--- pipelines/presubmit-skip.mk
+++ pipelines/presubmit-skip.mk
@@ -1,23 +1,29 @@
+.PHONY: sanity
 sanity:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping sanity...)
 
+.PHONY: lint
 lint:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping lint...)
 
+.PHONY: build-image
 build-image:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping build-image...)
 
+.PHONY: pr-preview
 pr-preview:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping pr-preview...)
 
+.PHONY: pr
 pr:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping pr...)
 
+.PHONY: k8s
 k8s:
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
 	$(info skipping k8s...)
