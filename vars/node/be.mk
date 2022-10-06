c9ecc314fb4f46b95cfd74d65a05200c6e79a079
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-10-06 11:50:52 -0600

    feat(tunnel): add proxy name suffix

--- vars/node/be.mk
+++ vars/node/be.mk
@@ -1,4 +1,4 @@
 # frp
-FRP_SUBDOMAIN ?= $(shell echo kio-web-app-api$${HOSTNAME:+-$${HOSTNAME%.*}})
+export FRP_SUBDOMAIN ?= $(shell echo kio-web-app-api$${HOSTNAME:+-$${HOSTNAME%.*}})
 FRPC_INI_SUBDOMAIN_INFO ?= $(FRP_SUBDOMAIN)
 FRPC_INI_VAULT_KEY ?= .frpc-be.ini
--- vars/node/fe.mk
+++ vars/node/fe.mk
@@ -1,4 +1,4 @@
 # frp
-FRP_SUBDOMAIN ?= $(shell echo kio-web-app$${HOSTNAME:+-$${HOSTNAME%.*}})
+export FRP_SUBDOMAIN ?= $(shell echo kio-web-app$${HOSTNAME:+-$${HOSTNAME%.*}})
 FRPC_INI_SUBDOMAIN_INFO ?= $(FRP_SUBDOMAIN)
 FRPC_INI_VAULT_KEY ?= .frpc-fe.ini
