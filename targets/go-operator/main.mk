2e2ab7991fc18e779349ca7b511532a7e3229218
Job Céspedes Ortiz <jobcespedes@gmail.com>
2023-06-19 18:16:23 -0600

    feat(go): detect arch in shared go cache

--- targets/go-operator/main.mk
+++ targets/go-operator/main.mk
@@ -21,13 +21,13 @@ go-lint-install: ## Install go linter package
 .PHONY: go-lint
 go-lint: go-lint-install ## Verifies `golint` passes
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
-	bin/golangci-lint run --verbose
+	$(LOCAL_BIN)/golangci-lint run --verbose
 
 .PHONY: kio-go-cache
 kio-go-cache: ## Verifies `golint` passes
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
-	mkdir -p /shared/operator-sdk.$(OPERATOR_VERSION)/go/{bin,testbin,.cache}
-	test -h $${HOME:-~}/go || ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/ $${HOME:-~}/go
-	test -h bin || ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/bin/ bin
-	test -h testbin || ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/testbin/ testbin
-	test -h $${HOME:-~}/.cache || ln -s /shared/operator-sdk.$(OPERATOR_VERSION)/go/.cache/ $${HOME:-~}/.cache
+	mkdir -p /shared/$(ARCH)/operator-sdk.$(OPERATOR_VERSION)/go/{bin,testbin,.cache}
+	test -h $${HOME:-~}/go || ln -s /shared/$(ARCH)/operator-sdk.$(OPERATOR_VERSION)/go/ $${HOME:-~}/go
+	test -h bin || ln -s /shared/$(ARCH)/operator-sdk.$(OPERATOR_VERSION)/go/bin/ bin
+	test -h testbin || ln -s /shared/$(ARCH)/operator-sdk.$(OPERATOR_VERSION)/go/testbin/ testbin
+	test -h $${HOME:-~}/.cache || ln -s /shared/$(ARCH)/operator-sdk.$(OPERATOR_VERSION)/go/.cache/ $${HOME:-~}/.cache
