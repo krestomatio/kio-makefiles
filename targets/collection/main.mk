bdddb40b0d24b8b05fd6c888c7d6de0f83b36778
Job Céspedes Ortiz <jobcespedes@gmail.com>
2024-03-16 14:11:25 -0600

    refactor: remove galaxy collection download verification
    galaxy endpoints changed

--- targets/collection/main.mk
+++ targets/collection/main.mk
@@ -2,8 +2,6 @@
 
 GALAXY_COLLECTION_NAME ?= krestomatio-k8s-$(VERSION)
 GALAXY_COLLECTION_FILE ?= $(GALAXY_COLLECTION_NAME).tar.gz
-GALAXY_COLLECTION_DOWNLOAD_URL ?= https://galaxy.ansible.com/download/$(GALAXY_COLLECTION_FILE)
-GALAXY_COLLECTION_PUBLISHED := $(shell curl --output /dev/null --silent --head --fail "$(GALAXY_COLLECTION_DOWNLOAD_URL)" && echo true || echo false)
 
 .PHONY: galaxy-version
 galaxy-version: ## Bump galaxy version
@@ -14,13 +12,9 @@ galaxy-version: ## Bump galaxy version
 .PHONY: galaxy-publish
 galaxy-publish: ## Publish galaxy collection
 	@echo -e "${LIGHTPURPLE}+ make target: $@${RESET}"
-ifeq ($(GALAXY_COLLECTION_PUBLISHED),false)
 	ansible-galaxy collection build --force
 	@echo -e "${YELLOW}++ ansible-galaxy collection publish krestomatio-k8s-$(VERSION).tar.gz${RESET}"
 	@ansible-galaxy collection publish krestomatio-k8s-$(VERSION).tar.gz --api-key $(ANSIBLE_GALAXY_TOKEN)
-else
-	@echo -e "not publisging $(GALAXY_COLLECTION_FILE), it already already is"
-endif
 
 .PHONY: ansible-lint
 ansible-lint: ## Ansible linting
